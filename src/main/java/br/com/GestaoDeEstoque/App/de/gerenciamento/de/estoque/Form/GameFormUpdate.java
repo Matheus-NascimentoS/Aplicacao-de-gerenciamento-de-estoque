0c4bd86a7c7ac4a11fcdbca0f5b95920bacc3262
Matheus-dev58 <matheus.nita78@gmail.com>
2021-09-20 15:32:29 -0300

    Uma classe form update foi criada para atualizar os jogos

---
+++ src/main/java/br/com/GestaoDeEstoque/App/de/gerenciamento/de/estoque/Form/GameFormUpdate.java
@@ -0,0 +1,34 @@
+package br.com.GestaoDeEstoque.App.de.gerenciamento.de.estoque.Form;
+
+public class GameFormUpdate {
+	
+	private int quantidadeEstoque;
+	private Double preco;
+	
+	public int getQuantidadeEstoque() {
+	
+		return quantidadeEstoque;
+	
+	}
+	
+	public void setQuantidadeEstoque(int quantidadeEstoque) {
+	
+		this.quantidadeEstoque = quantidadeEstoque;
+	
+	}
+	
+	public Double getPreco() {
+	
+		return preco;
+	
+	}
+	
+	public void setPreco(Double preco) {
+	
+		this.preco = preco;
+	
+	}
+	
+	
+
+}
